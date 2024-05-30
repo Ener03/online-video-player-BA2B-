43ebe36b434eeff30e1ae9f4c5fb027410b58120
Ener03 <154237274+Ener03@users.noreply.github.com>
2024-05-30 15:30:25 +0100

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.obsez.android.lib.filechooser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.obsez.android.lib.filechooser";
+  public static final String BUILD_TYPE = "debug";
+}
